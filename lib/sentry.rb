ad76e9cf8c498e1c2768dc80ac5687fdd5e0d2d9
Schanihbg <schani@live.se>
2016-10-27 18:52:25 +0200

    Added enable or disable in config, added identify Also added youtube and vimeo config

--- lib/sentry.rb
+++ lib/sentry.rb
@@ -4,6 +4,7 @@ require 'cinch/plugins/sentry/titles'
 require 'cinch/plugins/sentry/github'
 require 'cinch/plugins/sentry/reddit'
 require 'cinch/plugins/sentry/videos'
+require "cinch/plugins/identify"
 
 module Sentry
   module_function
@@ -25,13 +26,33 @@ module Sentry
           Cinch::Plugins::Sentry::Titles,
           Cinch::Plugins::Sentry::GitHub,
           Cinch::Plugins::Sentry::Reddit,
-          Cinch::Plugins::Sentry::Videos]
-        c.plugins.options = {
-          Cinch::Plugins::Sentry::Reddit => {
+          Cinch::Plugins::Sentry::Videos,
+          Cinch::Plugins::Identify]
+        c.plugins.options = {}
+
+        if config["plugins"]["identify"]["enabled"] then
+          c.plugins.options[Cinch::Plugins::Identify] = {
+            :password => config["plugins"]["identify"]["password"],
+            :type => :nickserv
+          }
+        end
+        if config["plugins"]["reddit"]["enabled"] then
+          c.plugins.options[Cinch::Plugins::Sentry::Reddit] = {
             "username" => config["plugins"]["reddit"]["username"],
             "password" => config["plugins"]["reddit"]["password"]
           }
-        }
+        end
+        if config["plugins"]["youtube"]["enabled"] then
+          c.plugins.options[Cinch::Plugins::Sentry::Videos] = {
+            "youtube" => config["plugins"]["youtube"]["apikey"]
+          }
+        end
+        if config["plugins"]["vimeo"]["enabled"] then
+          c.plugins.options[Cinch::Plugins::Sentry::Videos] = {
+            "vimeo" => config["plugins"]["vimeo"]["apikey"]
+          }
+        end
+
       end
     end
 
